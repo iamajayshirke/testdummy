f95b69f43c45257861b38953b932550f6c87aeab
Ajay Shirke <ajay.shirke@fugetroncorp.com>
2022-06-22 21:35:19 +0530

    new <p> tag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
        <p>Here We Go Git Modification And Staging Tutorial</p>
        <p>Git Logs: To Track All Commits</p>
+       <p>take it as new changes</p>
        <p>Git Status: To  Areas</p>
        New Sudden Modification
       </header>
