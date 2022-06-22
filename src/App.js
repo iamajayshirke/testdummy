5082dcadeecf0a818c350a6ea466b74d2548b64f
Ajay Shirke <ajay.shirke@fugetroncorp.com>
2022-06-22 21:24:02 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
        <p>Here We Go Git Tutorial</p>
        <p>Git Logs: To Track All Commits</p>
-       <p> Staging ArGit Status: To Modification Andeas</p>
+       <p>Git Status: To Modification And Staging Areas</p>
        New Sudden Modification
       </header>
     </div>
