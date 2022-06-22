b229fb9eb93ed4a8eb05ae4441109089af5bbc69
Ajay Shirke <ajay.shirke@fugetroncorp.com>
2022-06-22 20:42:44 +0530

    src folder changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Here We Go Git Tutorial</p>
+       <p>Git Logs: To Track All Commits</p>
+       <p>Git Status: To Track Modification And Staging Area</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Feature1.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Feature1() {
+  return (
+<<<<<<< HEAD
+    <div>Feature1
+        <p>New Changes Here</p>
+    </div>
+=======
+    <>
+    <div>Feature1</div>
+    <h1>New Features Here</h1>
+    </>
+>>>>>>> dev
+  )
+}
+
+export default Feature1
